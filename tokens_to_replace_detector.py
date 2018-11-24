04c91cf9410a71b2921f6b29cc97e9adb79b8eb4
eugenius <eugen.cernei@gmail.com>
2018-11-24 17:17:24 +0200

    Packed the additional code in TokenParser.

--- tokens_to_replace_detector.py
+++ tokens_to_replace_detector.py
@@ -3,24 +3,25 @@ from tone_analyzer import ToneAnalyzer
 
 class TokenParser:
 
-    def __init__(self):
+    def __init__(self, tone_analyzer = ToneAnalyzer()):
         self.nlp = spacy.load('en_core_web_lg')
-        # self.nlp = spacy.load('en')
+        self.tone_analyzer = tone_analyzer
 
-    def get_tokens(self, text, word_list):
+    def get_tokens(self, text):
+        bad_words = self.tone_analyzer.get_bad_words(text)
+        split_words = []
+        for word in bad_words:
+            split_words += word[0].split(" ")    
+        return  self._tokenize(text, split_words)
+
+    def _tokenize(self, text, word_list):
         doc = self.nlp(text)
         return [token for token in doc if token.text in word_list]
 
 if __name__ == "__main__":
     
     text = "Yo nigga, what's up, get your shit together, you whiny bitch."
-    
-    analyzer = ToneAnalyzer()
-    bad_words = analyzer.get_bad_words(text)
-    split_words = []
-    for word in bad_words:
-        split_words += word[0].split(" ")
-
+        
     parser = TokenParser()    
-    tokens = parser.get_tokens(text, split_words)
+    tokens = parser.get_tokens(text)
     print(tokens)

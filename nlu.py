9e502e8b7c749164b569cb93fe08ce7e53f21e6a
Alexandru Constantin <alex@devmotion.ro>
2018-11-24 14:40:06 +0200

    Nlu sentiment for text

--- nlu.py
+++ nlu.py
@@ -1,7 +1,7 @@
 import yaml
 
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
-from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions
+from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
 
 config = yaml.load(open('config.yaml', 'r'))
 nlu_config = config['IBM_NLU']
@@ -14,8 +14,22 @@ natural_language_understanding = NaturalLanguageUnderstandingV1(
 
 
 def do_nlu(text):
+    # nlu_analysis = natural_language_understanding.analyze(
+    #     text=text,
+    #     features=Features(entities=EntitiesOptions(sentiment=True, limit=1))).get_result()
+
     nlu_analysis = natural_language_understanding.analyze(
         text=text,
-        features=Features(entities=EntitiesOptions(sentiment=True, limit=1))).get_result()
-
+        features=Features(
+            entities=EntitiesOptions(
+                emotion=True,
+                sentiment=True,
+                limit=2),
+            keywords=KeywordsOptions(
+                emotion=True,
+                sentiment=True,
+                limit=2))).get_result()
     return nlu_analysis
+
+
+do_nlu('Ariana is a stupid bitch')

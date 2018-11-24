b49b756c99554c6806eab01da40a41dd5dbaf0c2
Alexandru Constantin <alex@devmotion.ro>
2018-11-24 16:29:22 +0200

    Implemented deoffensate using antonym approach

--- paraphraser.py
+++ paraphraser.py
@@ -1,10 +1,18 @@
-from tone_analyzer import analyze_tone
+import spacy
+
+from deoffensate_antonym import deoffensate_word_antonym_approach
+
+nlp = spacy.load('en_core_web_lg')
 
 
-def paraphrase(text):
-    tone_analysis = analyze_tone(text)
+def noho_tokens(spacy_tokens):
+    for spacy_token in spacy_tokens:
+        deoffensated_word = deoffensate_word_antonym_approach(spacy_token, nlp)
+        print(spacy_token.text, '<=>', deoffensated_word)
     return None
 
 
-text = 'You are a stupid bitch'
-paraphrase(text)
+if __name__ == '__main__':
+    blob = 'stupid bitch dickhead motherfucker imbecile retarded'
+    tokens = nlp(blob)
+    noho_tokens(tokens)

d68818b960a07a3fb28b5efa6ef7073c5d98d55c
Alexandru Constantin <alex@devmotion.ro>
2018-11-24 12:52:22 +0200

    Added todo endpoint

--- server.py
+++ server.py
@@ -7,9 +7,16 @@ from tone_analyzer import analyze_tone
 app = Flask(__name__)
 
 
-@app.route('/noho')
-def noho():
+@app.route('/tone')
+def tone():
     text = request.args.get('q')
     tone_analysis = analyze_tone(text)
 
     return json.dumps(tone_analysis)
+
+
+@app.route('/noho')
+def noho():
+    text = request.args.get('q')
+    tone_analysis = analyze_tone(text)
+    # TODO implement the alternative phrase generation

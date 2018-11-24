d0dcfdedbe5f94eb16a32f883301a5182e5b276e
eugenius <eugen.cernei@gmail.com>
2018-11-24 15:39:12 +0200

    Created basic structure of tokens_to_replace_detector

--- tokens_to_replace_detector.py
+++ tokens_to_replace_detector.py
@@ -1,10 +1,18 @@
 import spacy
 
+class TokenParser:
 
-blob = 'Ariana is a stupid bitch'
-nlp = spacy.load('en_core_web_sm')
-doc = nlp(blob)
+    def __init__(self):
+        nlp = spacy.load('en_core_web_lg')
 
-for token in doc:
-    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-          token.shape_, token.is_alpha, token.is_stop)
+    def get_tokens(self, str):
+        doc = nlp(blob)
+        for token in doc:
+            print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+                token.shape_, token.is_alpha, token.is_stop)
+
+
+if __name__ == "__main__":
+    blob = 'Ariana is a stupid bitch'
+    parser = TokenParser()    
+    parser.get_tokens(blob)

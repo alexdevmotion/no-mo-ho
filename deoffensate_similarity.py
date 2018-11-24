2bc82340f3f945d40faf18b1733d0d2bb36dca17
Alexandru Constantin <alex@devmotion.ro>
2018-11-24 21:01:16 +0200

    Update paraphraser to generate proper with empty tokens

--- deoffensate_similarity.py
+++ deoffensate_similarity.py
@@ -6,12 +6,17 @@ def deoffensate_word_similarity_approach(text, token, nlp, token_parser):
     tokens = [nlp(word)[0] for word in most_similar_words]
     tokens_filtered_by_pos = filter_tokens_by_pos(tokens, [token.pos_])
 
-    deoffensated_words = []
+    deoffensated_tokens = []
     for replacement_token in tokens_filtered_by_pos:
-        new_text = text.replace(token.text, replacement_token.text)
-        new_offensive_tokens = token_parser.get_offensive_tokens(new_text)
-        new_offesive_token_texts = [token.text for token in new_offensive_tokens]
-        if replacement_token.text not in new_offesive_token_texts:
-            deoffensated_words.append(replacement_token.text)
+        if not is_still_offensive(text, token.text, replacement_token.text, token_parser):
+            deoffensated_tokens.append(replacement_token)
             print(replacement_token.text, 'REPLACES', token.text)
-    return deoffensated_words
+    # TODO remove by lemmatization
+    return [token.text for token in deoffensated_tokens]
+
+
+def is_still_offensive(original_text, token_text, replacement_text, token_parser):
+    new_text = original_text.replace(token_text, replacement_text)
+    new_offensive_tokens = token_parser.get_offensive_tokens(new_text)
+    new_offesive_token_texts = [token.text for token in new_offensive_tokens]
+    return replacement_text in new_offesive_token_texts

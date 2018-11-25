c7124990faac1103bab82ae46d963c33eca0bd86
Alexandru Florian Barascu <alex.florin2352@gmail.com>
2018-11-25 15:45:20 +0200

    Last fixes.

--- frontend/server/websocket.js
+++ frontend/server/websocket.js
@@ -7,9 +7,13 @@ wss.on("connection", function (ws) {
 
     ws.on('message', async function(msg){
         console.log('received', msg);
-        // const result = await axios.get(`https://short-sloth-87.localtunnel.me/noho?${msg}`);
-        const result = await axios.get(`http://localhost:5000/noho?${msg}`);
-        ws.send(JSON.stringify(result.data));
+        try {
+          const result = await axios.get(`http://localhost:5000/noho?${msg}`);
+          // const result = await axios.get(`https://short-sloth-87.localtunnel.me/noho?${msg}`);
+          ws.send(JSON.stringify(result.data));
+        } catch (e) {
+          ws.send(JSON.stringify(['Sorry, something went wrong with the server', 'So here are some hardcoded strings']));
+        }
     });
 
     ws.on('close', function() {

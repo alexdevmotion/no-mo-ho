effc2c2e6292caf32a04b680ea19769292dc7f57
Alexandru Florian Barascu <alex.florin2352@gmail.com>
2018-11-25 12:58:41 +0200

    Server fixes.

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -4,10 +4,13 @@ const fs = require('fs');
 const https = require('https');
 const app = express();
 
+const PORT = 80;
+const HOSTNAME = '0.0.0.0';
+
 app.get('/noho', async function (req, res) {
   console.log(req.query);
   const result = await axios.get(`http://localhost:5000/noho?q=${req.query.q}`);
-  console.log(result);
+  console.log(result.data);
   res.send(result.data);
 });
 
@@ -15,6 +18,6 @@ https.createServer({
   key: fs.readFileSync('server.key'),
   cert: fs.readFileSync('server.cert')
 }, app)
-  .listen(80, function () {
-    console.log('Node dispatcher listening on port 80! Go to https://localhost:80/')
+  .listen(PORT, HOSTNAME, function () {
+    console.log(`Example app listening on port ${PORT}! Go to https://${HOSTNAME}:${PORT}/`)
   });
